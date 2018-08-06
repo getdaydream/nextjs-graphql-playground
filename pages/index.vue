c5141e084669eeebad11689674af7e4b033b3a19
getdaydream <getdaydream@gmail.com>
2018-08-06 16:29:52 +0800

    重置：使用nuxt后端渲染

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <div>
+      主页
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
+
