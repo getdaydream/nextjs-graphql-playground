140b28a696d523e766ec62db14536b6a54d4ff14
getdaydream <getdaydream@gmail.com>
2018-08-07 19:08:15 +0800

    chore

--- pages/movies/_id.vue
+++ pages/movies/_id.vue
@@ -7,9 +7,11 @@
 
 <script>
 import axios from 'axios';
+import store from '../../store';
 
 export default {
- async asyncData ({params}) {
+ async asyncData ({params, req}) {
+   console.error(req.headers)
     const {data:movie} = await axios.get(`http://127.0.0.1:3001/api/movies/${params.id}`);
     return {movie}
   },
--- store/index.js
+++ store/index.js
@@ -4,15 +4,6 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = () => new Vuex.Store({
-
-  state: {
-    token: ''
-  },
-  mutations: {
-    setToken(state, token) {
-      state.token = token;
-    }
-  }
 })
 
 export default store
