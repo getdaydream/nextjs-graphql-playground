fef7d50042b89ab744c34329dcb5788166520d91
getdaydream <getdaydream@gmail.com>
2018-08-20 21:35:59 +0800

    添加编辑图片信息功能

---
+++ components/tag-select.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-select
+    v-model="tags"
+    multiple
+    filterable
+    allow-create
+    default-first-option
+    placeholder="请选择标签"
+    @change="tagsChange">
+    <el-option
+      v-for="tag in tagOptions"
+      :key="tag.id"
+      :label="tag.name"
+      :value="tag.id"/>
+  </el-select>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- pages/upload/index.vue
+++ pages/upload/index.vue
@@ -1,41 +1,56 @@
 <template>
   <div class="container">
-    <el-upload
-      action=""
-      list-type="picture-card"
-      accept="image/jpg, image/jpeg, image/png, image/gif"
-      :file-list="fileList"
-      :auto-upload="false"
-      :on-change="handleChange"
-      :on-preview="handlePictureCardPreview"
-      :on-remove="handleRemove">
-      <i class="el-icon-plus"/>
-    </el-upload>
-    <el-dialog :visible.sync="dialogVisible">
-      <img
-        width="100%"
-        :src="dialogImageUrl"
-        alt="">
-    </el-dialog>
-
     <button
       class="btn btn-dark"
       @click="publish">
       发布图片
     </button>
+    <div class="upload">
+      <div class="upload-left">
+        <el-upload
+          action=""
+          accept="image/jpg, image/jpeg, image/png, image/gif"
+          list-type="picture"
+          :file-list="fileList"
+          :auto-upload="false"
+          :on-change="handleChange"
+          :on-remove="handleRemove"
+          :on-preview="handlePreview">
+          <app-button>点击上传</app-button>
+        </el-upload>
+      </div>
+      <div class="upload-right">
+        <template v-if="inspectedFile">
+          <img
+            :src="inspectedFile.url"
+            class="preview-image"
+            style="display:block;">
+          标题
+          <el-input v-model="fileDataMap[inspectedFile.uid].title"/>
+          描述
+          <el-input v-model="fileDataMap[inspectedFile.uid].description"/>
+
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import * as qiniu from 'qiniu-js';
 import { http } from '../../util/http';
+import AppButton from '../../components/app-button.vue';
 
 export default {
+  components: {
+    AppButton,
+  },
   data() {
     return {
       fileList: [],
-      dialogImageUrl: '',
-      dialogVisible: false,
+      inspectedFile: null,
+
+      fileDataMap: {},
     };
   },
   methods: {
@@ -64,12 +79,33 @@ export default {
     handleRemove(file) {
       const index = this.fileList.findIndex(f => f.uid === file.uid);
       this.fileList.splice(index, 1);
+      this.inspectedFile = null;
+    },
+    handlePreview(file) {
+      console.log(file);
+      this.inspectedFile = file;
+      const { uid } = file;
+      if (!this.fileDataMap[uid]) {
+        this.fileDataMap[uid] = {
+          title: file.name.split('.')[0],
+          size: file.size,
+          description: '',
+        };
+      }
     },
     async upload(file) {
       const { data } = await http.get('/files/uploadToken');
       const { token } = data;
       const { uid } = file;
-      const observable = qiniu.upload(file.raw, uid, token, {}, {});
+      const ext = file.name.split('.').pop();
+      const uuidv1 = require('uuid/v1');
+      const observable = qiniu.upload(
+        file.raw,
+        uuidv1() + '.' + ext,
+        token,
+        {},
+        {},
+      );
       // 注册回调
       const observer = {
         next: res => {
@@ -86,11 +122,28 @@ export default {
       // 开始上传
       observable.subscribe(observer);
     },
-    handlePictureCardPreview(file) {
-      this.dialogImageUrl = file.url;
-      this.dialogVisible = true;
+    // 发布图片
+    publish() {
+      const files = this.fileList.map(f => {
+        const file = this.fileDataMap[f.uid] || {
+          title: f.name.split('.')[0],
+          size: f.size,
+          description: '',
+        };
+        return {
+          key: f.response.key,
+          mimeType: f.raw.type,
+          hash: f.response.hash,
+          size: f.response.size,
+          height: f.response.height,
+          width: f.response.width,
+          title: file.title,
+          description: file.description,
+        };
+      });
+      const params = { files };
+      http.post('/files', params);
     },
-    publish() {},
   },
 };
 </script>
