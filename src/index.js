faac7804bc07daa302080fbc892a4e8774991ca6
getdaydream <getdaydream@gmail.com>
2018-09-21 16:45:23 +0800

    reset

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import 'normalize.css';
-import './assets/css/global.css';
-
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/pages/routes.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import BasicLayout from './BasicLayout';
-
-export const getRoutes = () => {
-  return <BasicLayout />;
-};
