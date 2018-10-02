e198d13fd2146696a9c4ebb52f4566d6519d940f
getdaydream <getdaydream@gmail.com>
2018-10-02 22:07:35 +0800

    enable hot module replacement, support css file

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,8 +1,24 @@
 const paths = require('./paths');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
+const portfinder = require('portfinder');
 
-module.exports = {
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = (process.env.PORT && Number(process.env.PORT)) || 3000;
+
+const webpackDevConfig = {
   mode: 'development',
+  // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
+  // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    compress: true,
+    quiet: true,
+    // Enable HMR
+    hot: true,
+    host: HOST,
+  },
   entry: paths.appEntry,
   output: {
     filename: 'bundle.js',
@@ -26,11 +42,46 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: paths.appHtml,
     }),
+    // Enable HMR, 当前只对css有效；ts会刷新页面
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = PORT;
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err);
+    } else {
+      // publish the new Port, necessary for e2e tests
+      process.env.PORT = port;
+      // add port to devServer config
+      webpackDevConfig.devServer.port = port;
+
+      // Add FriendlyErrorsPlugin
+      webpackDevConfig.plugins.push(
+        new FriendlyErrorsPlugin({
+          compilationSuccessInfo: {
+            messages: [
+              `Your application is running here: http://${
+                webpackDevConfig.devServer.host
+              }:${port}`,
+            ],
+          },
+        }),
+      );
+
+      resolve(webpackDevConfig);
+    }
+  });
+});
