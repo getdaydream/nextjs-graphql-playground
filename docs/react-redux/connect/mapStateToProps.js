30383b9742fd781cfb1c666f58120e8ac0c1d637
Dong Haoran <donghaoran@palmax.cn>
2018-10-16 19:10:09 +0800

    添加 react-redux 源码

---
+++ docs/react-redux/connect/mapStateToProps.js
@@ -0,0 +1,15 @@
+import { wrapMapToPropsConstant, wrapMapToPropsFunc } from './wrapMapToProps';
+
+// 如果 `MapStateToProps` 是函数
+export function whenMapStateToPropsIsFunction(mapStateToProps) {
+  return typeof mapStateToProps === 'function'
+    ? wrapMapToPropsFunc(mapStateToProps, 'mapStateToProps')
+    : undefined;
+}
+
+// 如果 `MapStateToProps` 参数
+export function whenMapStateToPropsIsMissing(mapStateToProps) {
+  return !mapStateToProps ? wrapMapToPropsConstant(() => ({})) : undefined;
+}
+
+export default [whenMapStateToPropsIsFunction, whenMapStateToPropsIsMissing];
