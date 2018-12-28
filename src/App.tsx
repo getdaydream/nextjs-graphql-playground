bfa1acb065a296f803183784d6bbcffb38f13d5a
getdaydream <getdaydream@gmail.com>
2018-12-28 22:38:05 +0800

    test unstated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import { Provider } from 'react-redux';
+import { Provider as ReduxProvider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'unstated';
 import './assets/scss/global.scss';
+import { PostEntityContainer } from './containers';
 import { getRoutes } from './pages/routes';
 import store from './store';
 
 class App extends React.Component {
   public render() {
     return (
-      <Provider store={store}>
-        <HashRouter>{getRoutes()}</HashRouter>
+      <Provider inject={[new PostEntityContainer()]}>
+        <ReduxProvider store={store}>
+          <HashRouter>{getRoutes()}</HashRouter>
+        </ReduxProvider>
       </Provider>
     );
   }
