d8f89597f8473016218b242a811c2312da752eb7
Dong Haoran <donghaoran@palmax.cn>
2019-02-28 18:48:42 +0800

    temp

--- src/pages/Post/PostDetail/Article.tsx
+++ src/pages/Post/PostDetail/Article.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 class Article extends React.Component {
+  public containerRef: React.RefObject<HTMLDivElement>;
+  public editor: any;
+
+  constructor(props: any) {
+    super(props);
+    this.containerRef = React.createRef();
+  }
+
+  public componentDidMount() {
+    //
+  }
+
   public render() {
-    return <div />;
+    return <div ref={this.containerRef} />;
   }
 }
 
