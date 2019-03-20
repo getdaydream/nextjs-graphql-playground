bbc1bf998131e0d124ddd61dda981983a42a2343
getdaydream <getdaydream@gmail.com>
2019-03-20 23:43:49 +0800

    try add jwt auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,26 @@ class Home extends React.Component {
       .query({
         query: gql`
           {
-            user(id: 1) {
+            me {
               id
               nickname
             }
           }
         `,
+        // query: gql`
+        //   query {
+        //     login(email: "1@qq.com", password: "12345678") {
+        //       user {
+        //         nickname
+        //       }
+        //       token
+        //     }
+        //   }
+        // `,
       })
       .then(result => {
         console.log(result);
+        // localStorage.setItem('token', result.data.login.token);
       });
   }
 
