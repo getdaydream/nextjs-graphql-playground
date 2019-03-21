e60308dad22d53c09c93ac59c7836f1f88f46180
getdaydream <getdaydream@gmail.com>
2019-03-21 21:56:14 +0800

    try jwt auth

---
+++ pages/login.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Login extends React.Component {
+  componentDidMount() {
+    //
+  }
+
+  handleClickLogin = () => {
+    // 
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClickLogin}>Login</button>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- utils/initApolloClient.ts
+++ utils/initApolloClient.ts
@@ -7,49 +7,47 @@ import fetch from 'isomorphic-fetch';
 
 let apolloClient: ApolloClient<{}>;
 
-const createClient = (
-  initialState: any,
+const createClient = () =>
   // options: { getToken: () => string },
-) => {
-  // const { getToken } = options;
+  {
+    // const { getToken } = options;
 
-  // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
-  return new ApolloClient({
-    // ssrMode: process.browser, // Disables forceFetch on the server (so queries are only run once)
-    cache: new InMemoryCache().restore(initialState || {}),
-    uri: 'http://192.168.0.4:3000/graphql',
-    credentials: 'include',
-  });
-};
+    // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
+    return new ApolloClient({
+      // ssrMode: process.browser, // Disables forceFetch on the server (so queries are only run once)
+      cache: new InMemoryCache(),
+      uri: 'http://192.168.0.4:3000/graphql',
+      credentials: 'include',
+    });
+  };
 
 export const client = new ApolloClient({
-  // cache: new InMemoryCache(),
   fetch,
   fetchOptions: {
     credentials: 'include',
   },
-  uri: 'http://192.168.100.67:3000/graphql',
+  uri: 'http://192.168.0.4:3000/graphql',
   // This function is called on each request.
   // It takes a GraphQL operation and can return a promise.
   request: async (operation: Operation) => {
     operation.setContext({
       headers: {
-        authorization: `bearer `,
+        Authorization: `bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ijg4ODg4ODg4QHFxLmNvbSIsImlhdCI6MTU1MzE3MjQ4NSwiZXhwIjoxNTUzMjA4NDg1fQ.c1-6K3rVdCgCerhzblMPD1H7YhyBNuh3-RFStUOrk_M`,
       },
     });
   },
 });
 
-const initApolloClient = (initialState: any) => {
+const initApolloClient = () => {
   // Make sure to create a new client for every server-side request so that data
   // isn't shared between connections (which would be bad)
   if (typeof window === undefined) {
-    return createClient(initialState);
+    return createClient();
   }
 
   // Reuse client on the client-side
   if (!apolloClient) {
-    apolloClient = createClient(initialState);
+    apolloClient = createClient();
   }
   return apolloClient;
 };
