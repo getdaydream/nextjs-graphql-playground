a8f79a3dc441eb3570654f1ecc35a85891a4d68b
getdaydream <getdaydream@gmail.com>
2019-03-26 23:15:29 +0800

    temp

---
+++ pages/oauth.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NextContext } from 'next';
+
+class Oauth extends React.Component {
+  static async getInitialProps({ query }: NextContext) {
+    const { code } = query
+    if (code) {
+      
+    }
+  }
+
+  handleClickGithubLogin = () => {
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=f56fd54bf938e93a6166
+    `;
+  };
+
+  render() {
+    return <button onClick={this.handleClickGithubLogin}>Github login</button>;
+  }
+}
+
+export default Oauth;
