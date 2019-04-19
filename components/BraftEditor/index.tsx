5f37be60c2c57b6d0c7c39bca141d07e4c3ded2f
getdaydream <getdaydream@gmail.com>
2019-04-19 19:07:54 +0800

    temp

---
+++ components/BraftEditor/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import BaseEditor from 'braft-editor';
+import 'braft-editor/dist/index';
+
+class BraftEditor extends React.Component {
+  render() {
+    return <BaseEditor />;
+  }
+}
+
+export default BraftEditor;
