1d5631f94e13461c77057d94ce33d9d4cd9d7182
getdaydream <getdaydream@gmail.com>
2019-05-12 20:04:35 +0800

    temp

--- containers/Headers/index.tsx
+++ containers/Headers/index.tsx
@@ -1,48 +1,32 @@
 import React from 'react';
-import AuthModal from '../AuthModal';
 import { Box } from 'grommet';
-import { observer, inject } from 'mobx-react';
-import { MstStoreProps } from '@/stores';
 import { Button, Avatar } from 'antd';
-import dynamic from 'next/dynamic';
+import { Query } from 'react-apollo';
+import { QueryMe } from '@/graphql/user';
+import { IQueryMe } from '@/graphql/__generated-types__';
 
-const ArticleEdit = dynamic(() => import('@/containers/ArticleEdit'), {
-  ssr: false,
-});
+const Header: React.FC = () => {
+  return (
+    <Box direction="row" pad="small" style={{ background: 'white' }}>
+      <Query<IQueryMe> query={QueryMe}>
+        {({ data }) => {
+          if (data && data.me) {
+            return (
+              <Box direction="row" justify="between" fill>
+                <div />
+                <Box direction="row" justify="end" align="center">
+                  <Button>Just for test Post</Button>
+                  <Avatar />
+                </Box>
+              </Box>
+            );
+          }
 
-class Header extends React.Component<MstStoreProps> {
-  openPost = () => {
-    const { initArticleEdit } = this.props.store;
-    initArticleEdit();
-  };
+          return <Button>登录</Button>;
+        }}
+      </Query>
+    </Box>
+  );
+};
 
-  render() {
-    const {
-      account: { user },
-      globalHeader: { showAuthModal, setShowAuthModal },
-      articleEdit,
-    } = this.props.store;
-
-    return (
-      <Box direction="row" pad="small" style={{ background: 'white' }}>
-        {showAuthModal && <AuthModal />}
-
-        {user ? (
-          <Box direction="row" justify="between" fill>
-            <div />
-            <Box direction="row" justify="end" align="center">
-              <Button onClick={this.openPost}>Post</Button>
-              <Avatar />
-            </Box>
-          </Box>
-        ) : (
-          <Button onClick={() => setShowAuthModal(true)}>登录</Button>
-        )}
-
-        {articleEdit && <ArticleEdit />}
-      </Box>
-    );
-  }
-}
-
-export default inject(store => store)(observer(Header));
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import Header from '@/containers/Headers';
 import { NextFC } from 'next';
-import { QueryMe } from '@/graphql/user';
-import { IQueryMe } from '@/graphql/__generated-types__';
-import { initApolloClient } from '@/utils';
-import { parseCookies } from '@/utils/parse-cookies';
-import { IStoreSnapshotIn } from '@/stores';
 
 const Home: NextFC = () => {
   return (
@@ -20,23 +15,6 @@ const Home: NextFC = () => {
       </Link>
     </div>
   );
-};
-
-Home.getInitialProps = async (
-  ctx: any,
-): Promise<{ mstStore: IStoreSnapshotIn }> => {
-  const gqClient = initApolloClient({
-    initialState: {},
-    getToken: () => parseCookies(ctx.req).token,
-  });
-  try {
-    const response = await gqClient.query<IQueryMe>({ query: QueryMe });
-    return { mstStore: { account: { user: response.data.me } } };
-  } catch (e) {
-    return {
-      mstStore: {},
-    };
-  }
 };
 
 export default Home;
