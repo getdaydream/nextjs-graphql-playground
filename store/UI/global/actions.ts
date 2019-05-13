1ae51cc971f52da9bf2725b3e9a094b2f39b6a2f
getdaydream <getdaydream@gmail.com>
2019-05-13 21:28:48 +0800

    fix unhandle epic bug

--- store/UI/global/actions.ts
+++ store/UI/global/actions.ts
@@ -3,3 +3,5 @@ import { createStandardAction } from 'typesafe-actions';
 export const setGlobalOverlay = createStandardAction(
   '@global/globalOverlay/setGlobalOverlay',
 )<string>();
+
+export const noop = createStandardAction('noop')();
--- store/UI/global/epics.ts
+++ store/UI/global/epics.ts
@@ -1,9 +1,11 @@
 import { Epic } from 'redux-observable';
-import { RootAction, RootState } from 'typesafe-actions';
+import { RootAction, RootState, isActionOf } from 'typesafe-actions';
+import { filter } from 'rxjs/operators';
+import { noop } from './actions';
 
 // placeholder
 export const fetchNoopEpic: Epic<
   RootAction,
   RootAction,
   RootState
-> = actions$ => actions$.pipe();
+> = actions$ => actions$.pipe(filter(isActionOf(noop)));
