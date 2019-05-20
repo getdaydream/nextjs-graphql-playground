9ce4130988573a3466bfb321f2ce4dd6a02b4ce2
getdaydream <getdaydream@gmail.com>
2019-05-20 21:35:04 +0800

    temp

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 // next.config.js
 const withPlugins = require('next-compose-plugins');
 const withTypescript = require('@zeit/next-typescript');
+const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 
 // fix: prevents error when .css files are required by node
 if (typeof require !== 'undefined') {
-  require.extensions['.css'] = file => {}
+  require.extensions['.css'] = file => {};
 }
 
-module.exports = withPlugins([[withTypescript], [withCSS]]);
+module.exports = withPlugins([[withTypescript], [withSass], [withCSS]]);
